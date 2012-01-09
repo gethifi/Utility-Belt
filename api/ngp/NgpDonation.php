67c23a62d5887b204b1c4559454fd5b78508fbb8
Josh Lockhart <joshlockhart@gmail.com>
2012-01-09 12:08:00 -0500

    Update inline docs in NgpDonation class

--- api/ngp/NgpDonation.php
+++ api/ngp/NgpDonation.php
@@ -1,8 +1,32 @@
 <?php
 /**
  * NGP Donation
- * @author Josh Lockhart
- * @version 1.0
+ *
+ * @author      Josh Lockhart <josh@newmediacampaigns.com>
+ * @copyright   2012 New Media Campaigns
+ * @link        http://www.newmediacampaigns.com
+ * @version     1.0.0
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * USAGE:
  *
@@ -23,8 +47,7 @@
  *     'ExpYear' => '13',
  *     'ExpMonth' => '02'
  * ));
- * $result = $d->save();
- * if ( $result === 0 ) {
+ * if ( $d->save() ) {
  *     //Success
  * } else {
  *     if ( $d->hasErrors() ) {
@@ -194,7 +217,7 @@ class NgpDonation {
      * than zero, inspect the transaction result with `getResult()`. If this returns false,
      * you should check for data errors with `getErrors()` or an API fault with `getFault()`.
      *
-     * @return int|false
+     * @return bool
      */
     public function save() {
         if ( $this->isValid() === false ) {
@@ -208,7 +231,7 @@ class NgpDonation {
         try {
             $res = $this->client->PostVerisignTransaction($args);
             $this->result = new SimpleXMLElement($res->PostVerisignTransactionResult);
-            return (int)$this->result->VendorResult->Result;
+            return (int)$this->result->VendorResult->Result === 0;
         } catch ( SoapFault $e ) {
             $this->fault = $e;
             return false;
